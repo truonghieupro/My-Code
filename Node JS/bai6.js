9db0074ebe5e772e833df86397513e0ee202b71b
truonghieu <truonghieu30042000@gmail.com>
2021-11-05 14:17:11 +0700

    hàm

---
+++ Node JS/bai6.js
@@ -0,0 +1,23 @@
+function tinhtong(a,b)
+{
+    return a+b; 
+}
+var x=tinhtong(2,3);
+console.log(x);
+///////////////////
+function hello()
+{
+    console.log("Wellcome");
+}
+function goiham(fn)
+{
+    fn();
+}
+goiham(hello);
+/////////////////
+var tong=function()
+{
+    console.log("Hahaha");
+}
+tong();
+
