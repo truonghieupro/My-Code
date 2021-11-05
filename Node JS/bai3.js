c0e0bc7bc1ba8335036a865f3dd2adbe11cadcdd
truonghieu <truonghieu30042000@gmail.com>
2021-11-05 12:04:22 +0700

    tạo biến trong node js

---
+++ Node JS/bai3.js
@@ -0,0 +1,30 @@
+var n=10;
+var s="abc";
+console.log(n+s); // 10abc //
+
+var x=1, y=2;
+console.log(x+y); // 3 //
+
+var l="1", m="2";
+console.log(l+m); // 12 //
+
+l=parseInt(l);
+m=parseInt(m);
+console.log(l+m); // 3 //
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
