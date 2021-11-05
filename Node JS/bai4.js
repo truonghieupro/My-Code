a071e926e67c7fa0ba08cdaa21a9f7510e65ec21
truonghieu <truonghieu30042000@gmail.com>
2021-11-05 13:54:58 +0700

    cac ham co ban

---
+++ Node JS/bai4.js
@@ -0,0 +1,21 @@
+var diem = 3;
+if (diem>=5)
+{
+    console.log("do roi")
+}
+else
+{
+    console.log("tach roi");
+}
+
+for(var i=1; i<10; i++)
+{
+    console.log("dong so " +i);
+}
+
+var dong=1;
+while(dong<5)
+{
+    console.log("dong thu "+dong);
+    dong++;
+}
