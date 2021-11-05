1917087b5b7d18937485824d8f4f487df43ccc5a
truonghieu <truonghieu30042000@gmail.com>
2021-11-05 14:05:23 +0700

    mảng trong node js

---
+++ Node JS/bai5.js
@@ -0,0 +1,10 @@
+var mang = ["cam","xoai","chuoi"];
+console.log(mang[0]);
+console.log(mang.length);
+mang.push("man");
+console.log(mang.length);
+
+for(var i=0;i<mang.length;i++)
+{
+    console.log(mang[i]);
+}
